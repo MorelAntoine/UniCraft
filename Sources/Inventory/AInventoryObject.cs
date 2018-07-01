6d3794f8ebd89fe2bbcb7c8beb6cd961fae6e93a
MorelAntoine <antoinemorel@outlook.com>
2018-07-01 15:50:07 +0200

    Add InventorySystem and InventoryObject prototype

---
+++ Sources/Inventory/AInventoryObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace UniCraft.Inventory
+{
+	public abstract class AInventoryObject : ScriptableObject
+	{
+		////////////////////////////////
+		////////// Attributes //////////
+
+		////////// Basic information //////////
+		
+		[Header("Basic information")]
+		[SerializeField] private string _name;
+		[SerializeField, TextArea] private string _description;
+		
+		////////////////////////////////
+		////////// Properties //////////
+		
+		////////// Basic information //////////
+
+		public string Name
+		{
+			get { return _name; }
+		}
+		
+		public string Description
+		{
+			get { return _description; }
+		}
+	}
+}
