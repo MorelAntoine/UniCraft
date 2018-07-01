b2a6e56f8658ff42427da3ecc35f69bb18d7a779
MorelAntoine <antoinemorel@outlook.com>
2018-07-01 23:32:54 +0200

    Add DisableInInspector attribute

---
+++ Sources/Toolbox/Attribute/DisableInInspector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UniCraft.Toolbox.Attribute
+{
+	public class DisableInInspector : PropertyAttribute
+	{}
+
+	[CustomPropertyDrawer(typeof(DisableInInspector))]
+	public class DisableInInspectorDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			EditorGUI.BeginDisabledGroup(true);
+			EditorGUI.PropertyField(position, property, label);
+			EditorGUI.EndDisabledGroup();
+		}
+	}
+}
