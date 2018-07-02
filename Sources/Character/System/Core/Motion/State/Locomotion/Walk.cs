88612bf314f22013279612be1001e92a0fc35490
MorelAntoine <antoinemorel@outlook.com>
2018-07-02 16:39:29 +0200

    [CharacterSystem] Add Basic Walk State

--- Sources/Character/System/Core/Motion/State/Locomotion/Walk.cs
+++ Sources/Character/System/Core/Motion/State/Locomotion/Walk.cs
@@ -6,8 +6,19 @@ namespace UniCraft.Character.System.Core.Motion.State.Locomotion
 	[CreateAssetMenu(menuName = "UniCraft/Character/State/Locomotion/Walk")]
 	public class Walk : AMotionState
 	{
+		////////////////////////////////
+		////////// Attributes //////////
+
+		private Vector3 _movement = Vector3.zero;
+		
+		/////////////////////////////
+		////////// Methods //////////
+		
 		public override void Move(CharacterSystem cs, GamepadInputInformation inputInfos)
-		{}
+		{
+			cs.transform.Rotate(0f, inputInfos.MotionHorizontalAxisValue * cs.Attributes.TurnSpeed * Time.deltaTime, 0f);
+			cs.transform.Translate(0f, 0f, inputInfos.MotionVerticalAxisValue * cs.Attributes.WalkSpeed * Time.deltaTime, Space.Self);
+		}
 
 		public override void Move2D(CharacterSystem2D cs2D, GamepadInputInformation inputInfos)
 		{}
--- Sources/Character/System/Core/Profile/AttributesProfile.cs
+++ Sources/Character/System/Core/Profile/AttributesProfile.cs
@@ -17,6 +17,7 @@ namespace UniCraft.Character.System.Core.Profile
 		[Header("Locomotion settings")]
 		[SerializeField, Range(0f, 8f)] private float _jumpHeight = 1.5f;
 		[SerializeField, Range(0f, 10f)] private float _runSpeed = 5.4f;
+		[SerializeField, Range(30f, 120f)] private float _turnSpeed = 90f;
 		[SerializeField, Range(0f, 4f)] private float _walkSpeed = 1.8f;
 
 		////////////////////////////////
@@ -34,6 +35,12 @@ namespace UniCraft.Character.System.Core.Profile
 		{
 			get { return _runSpeed; }
 			set { _runSpeed = value; }
+		}
+
+		public float TurnSpeed
+		{
+			get { return _turnSpeed; }
+			set { _turnSpeed = value; }
 		}
 
 		public float WalkSpeed
