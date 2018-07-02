85bf1bb8ae3a4d8048d0038916256308cb6db0ef
MorelAntoine <antoinemorel@outlook.com>
2018-07-02 17:30:10 +0200

    remove unused parameter

--- Sources/Character/System/Core/Motion/State/Locomotion/Walk.cs
+++ Sources/Character/System/Core/Motion/State/Locomotion/Walk.cs
@@ -6,14 +6,6 @@ namespace UniCraft.Character.System.Core.Motion.State.Locomotion
 	[CreateAssetMenu(menuName = "UniCraft/Character/State/Locomotion/Walk")]
 	public class Walk : AMotionState
 	{
-		////////////////////////////////
-		////////// Attributes //////////
-
-		private Vector3 _movement = Vector3.zero;
-		
-		/////////////////////////////
-		////////// Methods //////////
-		
 		public override void Move(CharacterSystem cs, GamepadInputInformation inputInfos)
 		{
 			cs.transform.Rotate(0f, inputInfos.MotionHorizontalAxisValue * cs.Attributes.TurnSpeed * Time.deltaTime, 0f);
