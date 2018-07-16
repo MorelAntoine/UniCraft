acf8b77f9cb8e8689b9a52882ffdedee4c064fa6
MorelAntoine <antoinemorel@outlook.com>
2018-07-16 18:35:37 +0200

    Add PluggableFiniteStateMachine

---
+++ Sources/Toolbox/PluggableFiniteStateMachine/PluggableFiniteStateMachine.cs
@@ -0,0 +1,76 @@
+﻿using UniCraft.Attribute;
+using UniCraft.Toolbox.PluggableFiniteStateMachine.State;
+using UnityEngine;
+
+namespace UniCraft.Toolbox.PluggableFiniteStateMachine
+{
+	/// <inheritdoc/>
+	/// <summary>
+	/// Class used to hold all the mechanism of the pluggable finite state machine
+	/// </summary>
+	public class PluggableFiniteStateMachine : MonoBehaviour
+	{
+		///////////////////////////////
+		////////// Attribute //////////
+		///////////////////////////////
+		
+		//////////////////////////////////////
+		////////// Compute Resource //////////
+		
+		private AState _nextState;
+		
+		/////////////////////////////////
+		////////// Information //////////
+		
+		////////// Data //////////
+		
+		private object[] _datas;
+		
+		////////// State //////////
+		
+		[CustomHeader("Information")]
+		[SerializeField, DisableInInspector(1)] private AState _currentState;
+		[SerializeField, DisableInInspector(1)] private AState _previousState;
+		
+		/////////////////////////////
+		////////// Setting //////////
+
+		[CustomHeader("Setting")]
+		[SerializeField, IndentLevel(1)] private AState _entryState;
+		
+		////////////////////////////
+		////////// Method //////////
+		////////////////////////////
+		
+		/// <summary>
+		/// Attempts to transit to a next state
+		/// </summary>
+		public void AttemptToTransit()
+		{
+			_nextState = _currentState.AttemptToGetNextState(_datas);
+			
+			if (_nextState != null)
+			{
+				_previousState = _currentState;
+				_currentState = _previousState;
+			}
+		}
+
+		/// <summary>
+		/// Runs the current state
+		/// </summary>
+		public void RunCurrentState()
+		{
+			_currentState.Run(_datas);
+		}
+
+		/// <summary>
+		/// Updates the pluggable finite state machine datas
+		/// </summary>
+		/// <param name="datas">The new pluggable finite state machine datas</param>
+		public void UpdateDatas(params object[] datas)
+		{
+			_datas = datas;
+		}
+	}
+}
---
+++ Sources/Toolbox/PluggableFiniteStateMachine/State/AState.cs
@@ -0,0 +1,52 @@
+﻿using UniCraft.Attribute;
+using UnityEngine;
+
+namespace UniCraft.Toolbox.PluggableFiniteStateMachine.State
+{
+    /// <inheritdoc/>
+    /// <summary>
+    /// Base class to derived to create a state for the pluggable finite state machine
+    /// </summary>
+    public abstract class AState : ScriptableObject
+    {
+        ///////////////////////////////
+        ////////// Attribute //////////
+        ///////////////////////////////
+        
+        //////////////////////////////////////
+        ////////// Compute Resource //////////
+        
+        private AState _nextState;
+        
+        /////////////////////////////
+        ////////// Setting //////////
+        
+        [CustomHeader("Setting")]
+        [SerializeField, IndentLevel(1)] private Transition.Transition[] _transitions;
+
+        ////////////////////////////
+        ////////// Method //////////
+        ////////////////////////////
+        
+        /// <summary>
+        /// (!Can return null!) Attempts to get the next state by cheking all the conditions
+        /// </summary>
+        /// <param name="datas">The finite state machine datas</param>
+        public AState AttemptToGetNextState(object[] datas)
+        {
+            foreach (var t in _transitions)
+            {
+                _nextState = t.AttemptToGetNextState(datas);
+                if (_nextState != null)
+                    return (_nextState);
+            }
+            return (null);
+        }
+        
+        /// <summary>
+        /// Runs the action of the state
+        /// </summary>
+        /// <param name="datas">The finite state machine datas</param>
+        public abstract void Run(object[] datas);
+    }
+}
