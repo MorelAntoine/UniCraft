a9ec14c6eda8d0aca4085dbb8c976b6e68c47249
MorelAntoine <antoinemorel@outlook.com>
2018-07-17 15:19:17 +0200

    Rework Character Behaviour System

--- Sources/Character/Behaviour/ACharacterBehaviour.cs
+++ Sources/Character/Behaviour/ACharacterBehaviour.cs
@@ -3,25 +3,23 @@ using UnityEngine;
 
 namespace UniCraft.Character.Behaviour
 {
+	/// <inheritdoc/>
+	/// <summary>
+	/// Base class to create a character behaviour
+	/// </summary>
+	[DisallowMultipleComponent]
+	[RequireComponent(typeof(MotionStateMachine.MotionStateMachine))]
 	public abstract class ACharacterBehaviour : MonoBehaviour
 	{
 		///////////////////////////////
 		////////// Attribute //////////
 		///////////////////////////////
 		
-		///////////////////////////////
-		////////// Component //////////
-
-		[SerializeField] private MotionStateMachine.MotionStateMachine _motionStateMachine;
+		private MotionStateMachine.MotionStateMachine _motionStateMachine;
 
 		//////////////////////////////
 		////////// Property //////////
 		//////////////////////////////
-
-		/////////////////////////////////
-		////////// Information //////////
-		
-		////////// Motion State Machine //////////
 		
 		protected MotionInformation MotionInformation
 		{
@@ -32,6 +30,31 @@ namespace UniCraft.Character.Behaviour
 		////////// Method //////////
 		////////////////////////////
 
+		//////////////////////////////////////////////////
+		////////// ACharacterBehaviour Callback //////////
+		
+		/// <summary>
+		/// Initilizes the behaviour
+		/// </summary>
+		protected abstract void Initialize();
+		
+		/// <summary>
+		/// Updates the motion information for the motion state machine
+		/// </summary>
 		protected abstract void UpdateMotionInformation();
+		
+		////////////////////////////////////////////
+		////////// MonoBehaviour Callback //////////
+
+		private void Awake()
+		{
+			_motionStateMachine = GetComponent<MotionStateMachine.MotionStateMachine>();
+			Initialize();
+		}
+
+		private void Update()
+		{
+			UpdateMotionInformation();
+		}
 	}
 }
