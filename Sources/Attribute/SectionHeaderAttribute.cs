3abfb113a9131a5a89ee8ab39daf15c34b25dde4
MorelAntoine <antoinemorel@outlook.com>
2018-07-17 12:02:25 +0200

    Update SectionHeader line, more sweety

--- Sources/Attribute/SectionHeaderAttribute.cs
+++ Sources/Attribute/SectionHeaderAttribute.cs
@@ -43,7 +43,7 @@ namespace UniCraft.Attribute
 
 		private const float GuiHeight = 4f;
 		private const float GuiStartY = 10f;
-		private const float LabelAlignOffset = 8f;
+		private const float AlignOffset = 8f;
 		private const float LabelPadding = 4f;
 		private const float TmpSpaceAfter = 8f;
 		private const float TmpSpaceBefore = 5f;
@@ -86,25 +86,25 @@ namespace UniCraft.Attribute
 		private void DrawEmptyHeaderSection(Rect guiPosition)
 		{
 			guiPosition.height = 1f;
-			guiPosition.y += GuiStartY + _sectionHeaderAttribute.SpaceBefore;
-			GUI.Box(guiPosition, "");
+			guiPosition.y += GuiStartY + _sectionHeaderAttribute.SpaceBefore - AlignOffset;
+			GUI.Box(guiPosition, "", GUI.skin.horizontalSlider);
 		}
 
 		private void DrawHeaderSection(Rect guiPosition)
 		{
 			_labelFieldWidth = (guiPosition.width - _sectionHeaderAttribute.LabelSize.x) / 2f;
-			guiPosition.y += GuiStartY + _sectionHeaderAttribute.SpaceBefore;
+			guiPosition.y += GuiStartY + _sectionHeaderAttribute.SpaceBefore - AlignOffset;
 			
 			guiPosition.Set(guiPosition.xMin, guiPosition.yMin, _labelFieldWidth, 1);
-			GUI.Box(guiPosition, "");
+			GUI.Box(guiPosition, "", GUI.skin.horizontalSlider);
 			
-			guiPosition.Set(guiPosition.xMin + _labelFieldWidth + LabelPadding, guiPosition.yMin - LabelAlignOffset,
+			guiPosition.Set(guiPosition.xMin + _labelFieldWidth + LabelPadding, guiPosition.yMin,
 				_sectionHeaderAttribute.LabelSize.x, _sectionHeaderAttribute.LabelSize.y);
 			GUI.Label(guiPosition, _sectionHeaderAttribute.Label, EditorStyles.boldLabel);
 
 			guiPosition.Set(guiPosition.xMin + _sectionHeaderAttribute.LabelSize.x + LabelPadding,
-				guiPosition.yMin + LabelAlignOffset, _labelFieldWidth - LabelPadding - LabelPadding, 1);
-			GUI.Box(guiPosition, "");
+				guiPosition.yMin, _labelFieldWidth - LabelPadding - LabelPadding, 1);
+			GUI.Box(guiPosition, "", GUI.skin.horizontalSlider);
 		}
 	}
 }
