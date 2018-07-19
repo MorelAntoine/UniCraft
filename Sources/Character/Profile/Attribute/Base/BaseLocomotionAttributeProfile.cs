52af0ee68cebbab697e68490bf12e37f31934a3b
MorelAntoine <antoinemorel@outlook.com>
2018-07-19 11:03:03 +0200

    Update character profiles

---
+++ Sources/Character/Profile/Attribute/Base/BaseLocomotionAttributeProfile.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace UniCraft.Character.Profile.Attribute.Base
+{
+    [global::System.Serializable]
+    public class BaseLocomotionAttributeProfile
+    {
+        ///////////////////////////////
+        ////////// Attribute //////////
+        ///////////////////////////////
+
+        [SerializeField, Range(0f, 4f)] private float _crouchSpeed = 1.2f;
+        [SerializeField, Range(0f, 20f)] private float _glideSpeed = 6.8f;
+        [SerializeField, Range(0f, 6f)] private float _jumpHeight = 2.7f;
+        [SerializeField, Range(0f, 20f)] private float _runSpeed = 5.8f;
+        [SerializeField, Range(0f, 8f)] private float _walkSpeed = 2.4f;
+
+        //////////////////////////////
+        ////////// Property //////////
+        //////////////////////////////
+
+        public float CrouchSpeed
+        {
+            get { return _crouchSpeed; }
+            set { _crouchSpeed = value; }
+        }
+
+        public float GlideSpeed
+        {
+            get { return _glideSpeed; }
+            set { _glideSpeed = value; }
+        }
+
+        public float JumpHeight
+        {
+            get { return _jumpHeight; }
+            set { _jumpHeight = value; }
+        }
+
+        public float RunSpeed
+        {
+            get { return _runSpeed; }
+            set { _runSpeed = value; }
+        }
+
+        public float WalkSpeed
+        {
+            get { return _walkSpeed; }
+            set { _walkSpeed = value; }
+        }
+    }
+}
---
+++ Sources/Character/Profile/Information/Base/BasePersonalInformationProfile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace UniCraft.Character.Profile.Information.Base
+{
+	[global::System.Serializable]
+	public class BasePersonalInformationProfile
+	{
+		///////////////////////////////
+		////////// Attribute //////////
+		///////////////////////////////
+
+		[SerializeField] private string _name;
+
+		//////////////////////////////
+		////////// Property //////////
+		//////////////////////////////
+
+		public string Name
+		{
+			get { return _name; }
+		}
+	}
+}
