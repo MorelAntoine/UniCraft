79e6f371c84ff04deda3cc0d1354a91b2d53d077
MorelAntoine <antoinemorel@outlook.com>
2018-07-19 12:05:35 +0200

    Add Idle and Walk State

---
+++ Sources/Character/MotionStateMachine/State/Locomotion/IdleState.cs
@@ -0,0 +1,15 @@
+﻿using UniCraft.Character.System;
+using UnityEngine;
+
+namespace UniCraft.Character.MotionStateMachine.State.Locomotion
+{
+	[CreateAssetMenu(menuName = "UniCraft/Character/State/Locomotion/Idle")]
+	public class IdleState : AMotionState
+	{
+		protected override void Run2D(ACharacterSystem2D cs2D, MotionInformation mi)
+		{}
+
+		protected override void Run3D(ACharacterSystem3D cs3D, MotionInformation mi)
+		{}
+	}
+}
