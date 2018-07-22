b614d55403176c15c7ea609969ea88fb1fe6c217
MorelAntoine <antoinemorel@outlook.com>
2018-07-22 10:33:49 +0200

    Add AngularSpeedStationary attribute to Locomotion

--- Sources/Character/Profile/Attribute/LocomotionAttributeProfile.cs
+++ Sources/Character/Profile/Attribute/LocomotionAttributeProfile.cs
@@ -9,16 +9,29 @@ namespace UniCraft.Character.Profile.Attribute
         ////////// Attribute //////////
         ///////////////////////////////
 
+        [SerializeField, Range(60f, 140f)] private float _angularSpeed = 88f;
+        [SerializeField, Range(100f, 180f)] private float _angularSpeedStationary = 124f;
         [SerializeField, Range(0f, 4f)] private float _crouchSpeed = 1.2f;
         [SerializeField, Range(0f, 20f)] private float _glideSpeed = 6.8f;
         [SerializeField, Range(0f, 6f)] private float _jumpHeight = 2.7f;
         [SerializeField, Range(0f, 20f)] private float _runSpeed = 5.8f;
-        [SerializeField, Range(60f, 140f)] private float _turnSpeed = 88f;
         [SerializeField, Range(0f, 8f)] private float _walkSpeed = 2.4f;
 
         //////////////////////////////
         ////////// Property //////////
         //////////////////////////////
+
+        public float AngularSpeed
+        {
+            get { return _angularSpeed; }
+            set { _angularSpeed = value; }
+        }
+
+        public float AngularSpeedStationary
+        {
+            get { return _angularSpeedStationary; }
+            set { _angularSpeedStationary = value; }
+        }
 
         public float CrouchSpeed
         {
@@ -42,12 +55,6 @@ namespace UniCraft.Character.Profile.Attribute
         {
             get { return _runSpeed; }
             set { _runSpeed = value; }
-        }
-
-        public float TurnSpeed
-        {
-            get { return _turnSpeed; }
-            set { _turnSpeed = value; }
         }
 
         public float WalkSpeed
